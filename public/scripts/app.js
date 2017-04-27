9a5a815f4a6d110074b995008017054d98ee0f7b
Danton Nguyen <dantonnguyen88@gmail.com>
2017-04-26 17:59:45 -0700

    Add error handling for user inputs

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,26 +3,33 @@ $(document).ready(function () {
   $('#calculate-submit').on('click', function(event) {
     event.preventDefault();
 
+
     let $wage = $('#wage-input').val();
     let $hours = $('#hours-input').val();
 
-    let $weeklyEarnings = $wage * $hours;
-    let $monthlyEarnings = $weeklyEarnings * 4;
-    let $yearlyEarnings = $weeklyEarnings * 52;
+    if(!$wage){
+      alert("Please Enter a valid Wage");
+    }
+    if(!$hours){
+      alert("Please Enter a valid number of Hours");
+    }
 
-    let $weekRow = $(".weeklyTR")
-    let $monthRow = $(".monthlyTR")
-    let $yearRow = $(".yearlyTR")
+    if( $wage && $hours){
+      let $weeklyEarnings = $wage * $hours;
+      let $monthlyEarnings = $weeklyEarnings * 4;
+      let $yearlyEarnings = $weeklyEarnings * 52;
 
-    $weekRow.find(".hours").text($hours);
-    $monthRow.find(".hours").text($hours * 4);
-    $yearRow.find(".hours").text($hours * 52);
+      let $weekRow = $(".weeklyTR");
+      let $monthRow = $(".monthlyTR");
+      let $yearRow = $(".yearlyTR");
 
-    $weekRow.find(".earnings").text($weeklyEarnings);
-    $monthRow.find(".earnings").text($monthlyEarnings);
-    $yearRow.find(".earnings").text($yearlyEarnings);
+      $weekRow.find(".hours").text($hours);
+      $monthRow.find(".hours").text($hours * 4);
+      $yearRow.find(".hours").text($hours * 52);
 
-
-    alert(`clicked! ${$wage} ${$hours} ${$weeklyEarnings}`);
+      $weekRow.find(".earnings").text(`$${$weeklyEarnings.toFixed(2)}`);
+      $monthRow.find(".earnings").text(`$${$monthlyEarnings.toFixed(2)}`);
+      $yearRow.find(".earnings").text(`$${$yearlyEarnings.toFixed(2)}`);
+    }
   });
 });
